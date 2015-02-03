6ea3ae0da6efb5543d969c2795e21da9dabfe1b9
Nataliya <natamiller971@gmail.com>
2015-02-02 20:21:10 -0800

    weird world1

---
+++ weird_world.py
@@ -0,0 +1,31 @@
+# adding the letter + 'ya'
+print "Welcome to the weird world!"
+pig = "ya"
+
+# word = 'hi'
+# word = raw_input("Type any word: ")
+
+"""
+def weird_world(word):
+	while not word or word.isdigit():
+		word = raw_input("Your string must be not empty and not be a digit!")
+
+
+	print word[1:].lower() + word[0].lower() + pig
+
+weird_world(word)
+"""
+
+def weird_world():
+	word = raw_input("please type a word (please no numbers): ")
+	word = word.lower()
+	if len(word) <= 0:
+		weird_world()
+	for letter in word:
+		if letter in ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9") and not letter.isalpha():
+			weird_world()
+
+	print word[1:] + word[0] + pig
+	print "Welcome to the Weird World"
+
+weird_world()
